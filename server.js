ad2cc73340b569950ceccb05b2337d144e49f89d
Serdar <ssimsekler@yahoo.com>
2018-02-10 23:09:31 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const MAINTENANCE_MODE = false;
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server log.');
+        }
+    })
+    next();
+});
+
+app.use((req, res, next) => {
+    if (MAINTENANCE_MODE) {
+        res.render('maintenance.hbs');
+    } else {
+        next();
+    }
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
+hbs.registerHelper('screamIt', (text) => text.toUpperCase());
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website...'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.listen(3000);
